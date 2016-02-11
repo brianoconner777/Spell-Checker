38e80ace15e64a6a9b45e04622e9174a30a85707
Saurabh Garg <saurabhgarg@Saurabhs-MacBook-Air.local>
2016-02-11 18:38:17 +0530

    Training hash table defined

--- checker.py
+++ checker.py
@@ -6,5 +6,13 @@ a = file('train.txt').read()
 
 b = words(a)
 
-def words(txt): return re.findall('[a-z]+',text.lower())
+def words(txt):
+ return re.findall('[a-z]+',text.lower())
+
+def train(t):
+	has = collections.defaultdict(lambda : 1)
+	for f in t:
+		has[f] += 1
+	return has
+
 
